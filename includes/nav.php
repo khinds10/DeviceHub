a3544130a5d65a1adf0a80ccdbebfed464a1ecdf
Kevin Hinds <khinds10.work@gmail.com>
2018-05-23 20:34:38 +0000

    updated UI for devices hub

--- includes/nav.php
+++ includes/nav.php
@@ -5,16 +5,16 @@
       </div>
       <div class="btn-group btn-group-justified" role="group">
          <div class="btn-group" role="group">
-            <a type="button" href="weatherclock.php?device=weather-clock" class="btn btn-default">Living</a>
+            <a type="button" href="weatherclock.php?device=weather-clock" class="btn btn-default"><i class="glyphicon glyphicon-time"></i> Living</a>
          </div>
          <div class="btn-group" role="group">
-            <a type="button" href="weatherclock.php?device=weather-clock-white" class="btn btn-default">Bed</a>
+            <a type="button" href="weatherclock.php?device=weather-clock-white" class="btn btn-default"><i class="glyphicon glyphicon-time"></i> Bed</a>
          </div>
          <div class="btn-group" role="group">
-            <a type="button" href="weatherclock.php?device=weather-clock-duluth" class="btn btn-default">Duluth</a>
+            <a type="button" href="weatherclock.php?device=weather-clock-duluth" class="btn btn-default"><i class="glyphicon glyphicon-screenshot"></i> Duluth</a>
          </div>
          <div class="btn-group" role="group">
-            <a type="button" href="weatherclock.php?device=trip-computer" class="btn btn-default">Car</a>
+            <a type="button" href="weatherclock.php?device=trip-computer" class="btn btn-default"><i class="glyphicon glyphicon-road"></i> Car</a>
          </div>
       </div>
    </div>
--- index.php
+++ index.php
@@ -6,18 +6,19 @@ include 'includes/nav.php';
         <?php
         $devices = array('weather-clock', 'magic-mirror', 'weather-clock-white', 'weather-clock-duluth', 'trip-computer');
         foreach ($devices as $device) { 
+            $icon = "glyphicon glyphicon-time";
+            if ($device == 'weather-clock-duluth') $icon = "glyphicon glyphicon-screenshot";
+            if ($device == 'trip-computer') $icon = "glyphicon glyphicon-road";
         ?>
-        <h2><?=ucwords(preg_replace('/\-/',' ',$device))?></h2>
+                <h2> <i class='<?=$icon?>'></i> <?=ucwords(preg_replace('/\-/',' ',$device))?></h2>
 
                 <?php
                 // get most recent readings from device 
                 $sql = "SELECT * FROM devices WHERE `device` = '" . $device ."' ORDER BY `entry` DESC LIMIT 1";
 
                 $myResults = array();
-                if ($result = $db->query($sql)) {
-                    while($row = $result->fetch_array(MYSQLI_ASSOC)) $myResults[] = $row;
-                }
-                foreach ($myResults as $myResult) print "<h4 style='color:darkblue; padding-left:25px;'> Inside: " . $myResult['value1']. " *F / Inside: " . $myResult['value2']. " %</h4>";
+                if ($result = $db->query($sql)) while($row = $result->fetch_array(MYSQLI_ASSOC)) $myResults[] = $row;
+                foreach ($myResults as $myResult) print "<h4 style='color:darkblue; padding-left:25px;'>Inside: " . $myResult['value1']. " *F / Inside: " . $myResult['value2']. " %</h4>";
                 ?>
         <?php
         }
@@ -27,3 +28,4 @@ include 'includes/nav.php';
 <?php
 include 'includes/footer.php';
 ?>
+
